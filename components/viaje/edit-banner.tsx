9ef3091afd2949cc4090e968ed4707641f73de4d
Diego Tenjo <tenjodiego961@gmail.com>
2026-06-03 00:16:25 -0500

    Remove dated hidden banner message

--- components/viaje/edit-banner.tsx
+++ components/viaje/edit-banner.tsx
@@ -1,18 +1,4 @@
-"use client"
-
-import { useEffect, useState } from "react"
-
-const FECHA_LIMITE = new Date("2026-06-21T00:00:00-05:00").getTime()
-
 export function EditBanner() {
-  const [mostrar, setMostrar] = useState(false)
-
-  useEffect(() => {
-    setMostrar(Date.now() < FECHA_LIMITE)
-  }, [])
-
-  if (!mostrar) return null
-
   return (
     <div className="edit-banner">
       <div className="edit-dot" aria-hidden="true" />
@@ -22,10 +8,6 @@ export function EditBanner() {
           aquí hay un pueblo digno. Ha llegado el tiempo de los pueblos."
         </p>
         <cite>Aída Marina Quilcué</cite>
-        <span className="edit-secret" aria-hidden="true">
-          <span>Continuemos con el cambio</span>
-          <span>en segunda vuelta.</span>
-        </span>
       </blockquote>
     </div>
   )
